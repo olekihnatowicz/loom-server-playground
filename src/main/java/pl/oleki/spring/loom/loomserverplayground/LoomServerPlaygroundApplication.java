1593cd843b504b5a8680fa3ff306d9408d0be6e7
Aleksander Ihnatowicz <oleki@interia.pl>
2024-03-20 09:53:30 +0100

    initial commit

---
+++ src/main/java/pl/oleki/spring/loom/loomserverplayground/LoomServerPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package pl.oleki.spring.loom.loomserverplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomServerPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoomServerPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/oleki/spring/loom/loomserverplayground/LoomServerPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.oleki.spring.loom.loomserverplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoomServerPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
